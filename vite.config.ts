76be21124fdb613fad695bfe993a607135752e88
Rain7_osu <1789446861@qq.com>
2021-09-16 22:36:27 +0800

    修改了一些环境相关的东西

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,28 @@
-import { defineConfig } from 'vite';
+import { build, defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { proxyUrl, buildTarget } from './config.json'
+
+// @ts-ignore
+const mode = process.env.NODE_ENV;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://175.27.138.106:3008',
-        changeOrigin: true,
+export default defineConfig(() => {
+
+  return {
+    plugins: [reactRefresh()],
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: proxyUrl,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    target: 'es2015',
-    outDir: '../devhub-proxy/dist',
-    assetsDir: ''
-  },
+    build: {
+      outDir: buildTarget,
+      assetsInlineLimit: 4096,
+    },
+    mode: 'production',
+  }
 });
