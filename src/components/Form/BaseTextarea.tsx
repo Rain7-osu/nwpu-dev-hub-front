cbcd187e54478f3b410556625ab5f2814510b447
Rain7_osu <1789446861@qq.com>
2021-09-16 20:21:52 +0800

    【fix】更新部分bug

--- src/components/Form/BaseTextarea.tsx
+++ src/components/Form/BaseTextarea.tsx
@@ -1,4 +1,4 @@
-import React, { BaseSyntheticEvent } from 'react';
+import React, { BaseSyntheticEvent, useState } from 'react';
 import { BaseTextareaContainer } from './styles';
 
 export interface BaseTextareaProps {
@@ -7,12 +7,24 @@ export interface BaseTextareaProps {
   maxLength?: number;
 }
 
+const MAX_LENGTH = 400;
+
 export function BaseTextarea (props: BaseTextareaProps) {
   const { value, onInput, maxLength } = props;
+  const [count, setCount] = useState<number>(0);
+
+  const handleInput = (e: BaseSyntheticEvent) => {
+    setCount(e.target.value.length);
+    onInput && onInput(e);
+  }
 
   return (
     <BaseTextareaContainer>
-      <textarea maxLength={ maxLength || 200} onInput={onInput} value={value} />
+      <textarea maxLength={maxLength} onInput={handleInput} value={value} />
+      <div className="textarea-counter">
+        <span className="current-count">{count}</span>
+        {maxLength && <span> / {maxLength}</span>}
+      </div>
     </BaseTextareaContainer>
   );
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -53,9 +53,24 @@ export const InputContainer = styled.div`
 `;
 
 export const BaseTextareaContainer = styled.div`
+  position: relative;
+
+  .textarea-counter {
+    color: #444;
+    position: absolute;
+    bottom: -20px;
+    right: 10px;
+    height: 20px;
+    font-size: 14px;
+    
+    .current-count {
+      color: #f00;
+    }
+  }
+
   textarea {
-    line-height: 32px;
     padding: 20px;
+    line-height: 32px;
     resize: none;
     width: 100%;
     box-sizing: border-box;
@@ -63,14 +78,17 @@ export const BaseTextareaContainer = styled.div`
     font-family: -apple-system, "Microsoft YaHei UI", BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
+    border: none;
+    outline: none;
     ${baseInput};
   
-    @media screen and (max-width: 480px) {
-        height: 400px;
+    @media screen and (min-width: 480px) {
+        height: 200px;
     }
   
     @media screen and (max-width: 480px) {
         height: 400px;
     }
   }
-}`;
+`;
+
