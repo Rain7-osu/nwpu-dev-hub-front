5cff839afcaa2d97bd9d360643bcf200ca3a0863
Rain7_osu <1789446861@qq.com>
2021-09-18 21:34:09 +0800

    【feat】开启导航条

--- src/routes/Navigator/Navigator.tsx
+++ src/routes/Navigator/Navigator.tsx
@@ -8,10 +8,10 @@ const Links = () => {
   return (
     <LinkUl>
       <li><Link to="/">首页</Link></li>
-      {/*<li><a>社区简介</a></li>*/}
-      {/*<li><a>新闻中心</a></li>*/}
-      {/*<li><a>项目介绍</a></li>*/}
-      {/*<li><a>加入我们</a></li>*/}
+      <li><a>社区简介</a></li>
+      <li><a>新闻中心</a></li>
+      <li><a>项目介绍</a></li>
+      <li><a>加入我们</a></li>
       <li><Link to="/register">报名</Link></li>
     </LinkUl>
   );
